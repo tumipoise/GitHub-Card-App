97ac7c455785a69dc135131db11e1ff236b2e090
Tumininu Sosanya <tumininu.sosanya@gmail.com>
2020-09-18 16:48:18 -0700

    styling the components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class Card extends Component {
-    render() {
-      
-const profile = this.props
+  render() {
+    const profile = this.props;
 
     return (
+      <Fragment>
       <div className="github-profile">
         <img src={profile.avatar_url} />
         <div className="info">
-                <div className="name">{profile.name}</div>
-          <div className="company">{profile.bio}</div>
-          <div className="company">{profile.location}</div>
-          <div className="company">{profile.url}</div>
-          <div className="company">{profile.twitter}</div>
+          <div className="name">{profile.name}</div>
+          <div className="bio">{profile.bio}</div>
+          <div className="location">{profile.location}</div>
+          <div className="url">{profile.url}</div>
+          <div className="twitter">{profile.twitter}</div>
         </div>
       </div>
+        <hr className="line-break" />
+        </Fragment>
     );
+
   }
 }
 
